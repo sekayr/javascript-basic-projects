f839b6e57296737a2767a0fe35cf93840f98e435
Sekar Ayu <88611931+scskar@users.noreply.github.com>
2022-07-22 10:39:05 +0700

    sidebar practice done

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -5,4 +5,20 @@
 // toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
-const link = document.querySelector(".links");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //   console.log(links.classList);
+  //   console.log(links.classList.contains("random")); shows false
+  //   console.log(links.classList.contains("links")); shows true
+
+  //adding functionality when button clicked the menu shows
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  //or you can write one line of code using toggle with similar functionality
+  links.classList.toggle("show-links");
+});
